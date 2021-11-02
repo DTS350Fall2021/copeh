7757a1abd00a9fb75a955bb81843d1dc112bbd55
Hannah C <copeh.19@william.jewell.edu>
2021-11-02 11:18:08 -0500

    Task 16 Complete

--- Week9/Task16/Task16.R
+++ Week9/Task16/Task16.R
@@ -7,7 +7,7 @@ library(gridExtra)
 car_wash <- read_csv(url("https://github.com/WJC-Data-Science/DTS350/raw/master/carwash.csv"))
 head(car_wash)
 
-#Covnert times from UTC to mountain time
+#Convert times from UTC to mountain time
 CarWash <- with_tz(car_wash, tzone = "US/Mountain")
 head(CarWash)
 
@@ -16,14 +16,14 @@ new_CarWash <- CarWash %>%
   mutate(grouping_hour = ceiling_date(time, "hour"))
 head(new_CarWash)
 
-#Find the sum of the values over each hour ???
+#Find the sum of the values over each hour
 salesdata <- new_CarWash %>%
   mutate(hour = hour(time)) %>%
   group_by(hour) %>%
   summarise(amount = sum(amount, na.rm = TRUE))
 View(salesdata)
 
-#Get matching temperatures.
+#Get matching temperatures
 temps <- riem_measures(station = "RXE", date_start = "2016-05-13", date_end = "2016-07-08")
 View(temps)
 
@@ -54,3 +54,7 @@ tempplot <- ggplot(data = all_data, aes(x = grouping_hour, y = tmpf)) +
        title = "Temperatures During the Hours")
 
 grid.arrange(tempplot, hourplot, nrow = 2)
+# From this, I am not able to see any correlation that it very noticeable. The only point that I do see is that
+# when the temperatures get to a low at the end of May, the sales take a drop. But then I see that when sales 
+# are at there highest in mid-June, temperature are also pretty low, which is why I believe that there is no
+# correlation between temperatures and sales.
